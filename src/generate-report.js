4c15414934362e6667adc28e12b253e5f9568d11
MarioRivis <mario.rivis@gmail.com>
2022-06-15 15:32:38 +0300

    integrated with other modules

--- src/generate-report.js
+++ src/generate-report.js
@@ -4,6 +4,8 @@ import { print, usage } from "./functions.js";
 import ora from "ora";
 import child_process from "child_process";
 import chalk from "chalk";
+import {generateImages} from "@dxworks/echarts-to-pics";
+import {generateReport} from "@dxworks/report-generator/dist/lib.js";
 
 const spinner = ora('working');
 print('\n');
@@ -20,11 +22,15 @@ const directoryPath = args[2];
 const templateFile = args[3];
 //print(templateFile)
 
-var firstModule = child_process.fork('C:/Users/IoanaL/LICENTA/NodeImageGenerator/echarts-to-pics/src/generate-images.js', [directoryPath, 'jpg', '1000'], { silent:true })
+generateImages()
 
-firstModule.on('exit', function() {
+generateReport().then(() => console.log("Done"))
 
-    var secondModule = child_process.fork('C:/Users/IoanaL/LICENTA/NodePptGenerator/report-generator/lib/report-generator.js', [templateFile])
-
-    spinner.succeed(chalk.green(" finished\n"));
-});
+// var firstModule = child_process.fork('C:/Users/IoanaL/LICENTA/NodeImageGenerator/echarts-to-pics/src/generate-images.js', [directoryPath, 'jpg', '1000'], { silent:true })
+//
+// firstModule.on('exit', function() {
+//
+//     var secondModule = child_process.fork('C:/Users/IoanaL/LICENTA/NodePptGenerator/report-generator/lib/report-generator.js', [templateFile])
+//
+//     spinner.succeed(chalk.green(" finished\n"));
+// })
